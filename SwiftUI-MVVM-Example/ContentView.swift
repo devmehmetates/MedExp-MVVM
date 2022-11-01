aa3d8ef2124ed380f8ee1f832d20b93b67573cc6
Mehmet Ateş  <Mehmet1095@icloud.com>
2022-11-01 18:59:20 +0300

    Initial commit

---
+++ SwiftUI-MVVM-Example/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUI-MVVM-Example
+//
+//  Created by Mehmet Ateş on 1.11.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-MVVM-Example/SwiftUI_MVVM_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MVVM_ExampleApp.swift
+//  SwiftUI-MVVM-Example
+//
+//  Created by Mehmet Ateş on 1.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MVVM_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
