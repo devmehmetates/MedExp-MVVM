d67bba0a00a770a97acca3c8c04c64d5788dc495
Mehmet Ateş  <Mehmet1095@icloud.com>
2022-11-09 15:05:35 +0300

    refactoring media list section

--- SwiftUI-MVVM-Example/Components/MediaListSection.swift
+++ SwiftUI-MVVM-Example/Components/MediaListSection.swift
@@ -15,31 +15,38 @@ struct MediaListSection: View {
     
     var body: some View {
         VStack(alignment: .leading, spacing: 2.0.responsizeW) {
-            Text(sectionTitle)
-                .font(.title2)
-                .fontWeight(.semibold)
-                .padding(.horizontal, 5.0.responsizeW)
-            ScrollView(.horizontal, showsIndicators: false) {
-                LazyHStack {
-                    ForEach(Array(zip(mediaList.indices, mediaList)), id: \.1.id) { index, media in
-                        NavigationLink {
-                            if mediaType == .movie {
-                                
-                            } else {
-                                
-                            }
-                        } label: {
-                            MediaCardView(media: media)
-                                .onAppear {
-                                    if index == mediaList.count - 3 {
-                                        cardOnAppear?()
-                                    }
-                                }
-                        }
-                    }
-                }.padding([.horizontal, .bottom])
-            }
+            sectionTitleText
+            mediaHorizontalScrollList
         }
+    }
+}
+
+// MARK: - View Component(s)
+extension MediaListSection {
+    private var mediaHorizontalScrollList: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            LazyHStack {
+                ForEach(Array(zip(mediaList.indices, mediaList)), id: \.1.id) { index, media in
+                    NavigationLink {
+                        
+                    } label: {
+                        MediaCardView(media: media)
+                            .onAppear {
+                                if index == mediaList.count - 3 {
+                                    cardOnAppear?()
+                                }
+                            }
+                    }
+                }
+            }.padding([.horizontal, .bottom])
+        }
+    }
+    
+    private var sectionTitleText: some View {
+        Text(sectionTitle)
+            .font(.title2)
+            .fontWeight(.semibold)
+            .padding(.horizontal, 5.0.responsizeW)
     }
 }
 
