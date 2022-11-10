10d003d88012159913fd5e5ac1c5eb803b2bc7cb
Mehmet Ateş  <Mehmet1095@icloud.com>
2022-11-10 20:52:36 +0300

    detail view started

--- SwiftUI-MVVM-Example/Modules/Detail/DetailView.swift
+++ SwiftUI-MVVM-Example/Modules/Detail/DetailView.swift
@@ -6,12 +6,48 @@
 //
 
 import SwiftUI
+import StickyAsyncImageSwiftUI
 
 struct DetailView<Model>: View where Model: DetailViewModelProtocol {
     @ObservedObject var viewModel: Model
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Group {
+            if let media = viewModel.mediaDetail {
+                ScrollView {
+                    StickyAsyncImageSwiftUI(url: URL(string: media.backdropImage), size: 50.0.responsizeW, coordinateSpace: "sticky")
+                    VStack {
+                        HStack {
+                            AnimatedAsyncImageView(path: media.posterImage)
+                                .frame(width: 40.0.responsizeW, height: 60.0.responsizeW)
+                                .transformEffect(CGAffineTransform(1, 0, 0, 1, 0, -10.0.responsizeW))
+                            VStack(alignment: .leading) {
+                                Text(media.title)
+                                    .lineLimit(4)
+                                    .font(.system(size: 6.responsizeW, weight: .bold))
+                                Text(media.releaseYear)
+                                    .font(.footnote)
+                                Spacer()
+                            }
+                            Spacer()
+                        }.padding(.horizontal)
+                            .frame(height: 40.0.responsizeW)
+                    }
+                }.coordinateSpace(name: "sticky")
+                    .ignoresSafeArea(edges: .top)
+                    
+            } else {
+                createLoadingState()
+                    .onAppear {
+                        viewModel.handleMediaDetail()
+                    }
+                    .onAppear {
+                        viewModel.handleMediaDetail()
+                    }
+            }
+        }.onAppear {
+            viewModel.handleMediaDetail()
+        }
     }
 }
 
@@ -21,14 +57,38 @@ struct DetailView_Previews: PreviewProvider {
     }
 }
 
-protocol DetailViewModelProtocol: ObservableObject { }
+protocol DetailViewModelProtocol: ObservableObject {
+    var mediaDetail: Media? { get }
+    func handleMediaDetail()
+}
 
 class DetailViewModel: DetailViewModelProtocol {
+    @Published var mediaDetail: Media?
+    
     var mediaId: Int
     var mediaType: MediaTypes
     
     init(mediaId: Int, mediaType: MediaTypes) {
         self.mediaId = mediaId
         self.mediaType = mediaType
+    }
+    
+    func handleMediaDetail() {
+        let endpoint: String = mediaType == .tvShow ? ApiEndpoints.tvShowDetail.rawValue : ApiEndpoints.movieShowDetail.rawValue
+        
+        let url = NetworkManager.shared.createRequestURL(endpoint, pathVariables: [mediaId], headerParams: [
+            "api_key": AppEnvironments.apiKey
+        ])
+        NetworkManager.shared.apiRequest(endpoint: url) { response in
+            switch response {
+            case .success(let data):
+                guard let decodedData: Media = data.decodedModel() else { return }
+                DispatchQueue.main.async {
+                    self.mediaDetail = decodedData
+                }
+            case .failure:
+                print("err")
+            }
+        }
     }
 }
