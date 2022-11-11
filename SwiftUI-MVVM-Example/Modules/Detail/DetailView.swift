e3b6cd6520a79e7b1627110c88565f6f5cf583b4
Mehmet Ateş  <Mehmet1095@icloud.com>
2022-11-11 17:54:25 +0300

    detail view and viewmodel splitted

--- SwiftUI-MVVM-Example/Modules/Detail/DetailView.swift
+++ SwiftUI-MVVM-Example/Modules/Detail/DetailView.swift
@@ -17,57 +17,40 @@ struct DetailView<Model>: View where Model: DetailViewModelProtocol {
             if let media = viewModel.mediaDetail {
                 ScrollView {
                     StickyAsyncImageSwiftUI(url: URL(string: media.originalBackdropImage), size: 50.0.responsizeW, coordinateSpace: "sticky")
-                    VStack {
-                        HStack {
-                            AnimatedAsyncImageView(path: media.posterImage)
-                                .frame(width: 40.0.responsizeW, height: 60.0.responsizeW)
-                                .transformEffect(CGAffineTransform(1, 0, 0, 1, 0, -10.0.responsizeW))
-                            VStack(alignment: .leading) {
-                                Text(media.title)
-                                    .lineLimit(4)
-                                    .font(.system(size: 6.responsizeW, weight: .bold))
-                                Text(media.releaseYear)
-                                    .font(.footnote)
-                                Spacer()
-                                if let mediaHomePage: URL = URL(string: media.mediaHomePage) {
-                                    Button {
-                                        openURL(mediaHomePage)
-                                    } label: {
-                                        LogoImageView(media: media)
-                                    }
-                                }
-                            }
-                            Spacer()
-                        }.padding(.horizontal)
-                            .frame(height: 40.0.responsizeW)
-                    }
-                    VStack(spacing: 5.0.responsizeW) {
-                        VStack(spacing: 5) {
-                            HStack {
-                                Text("Overview")
-                                    .font(.largeTitle)
-                                Spacer()
-                            }
-                            
-                            Text(media.overview)
-                                .font(.caption)
-                        }.padding(.horizontal)
-                        
-                        VStack(spacing: 5) {
-                            HStack {
-                                Text("Actors")
-                                    .font(.largeTitle)
-                                Spacer()
-                            }.padding(.horizontal)
-                            ScrollView(.horizontal, showsIndicators: false) {
-                                LazyHStack {
-                                    ForEach(viewModel.mediaActors) { mediaActor in
-                                        ActorCardView(actor: mediaActor)
-                                    }
-                                }.padding([.horizontal, .bottom])
+                    createImageHeaderInformationStack(media: media)
+                    VStack(spacing: 3.0.responsizeW) {
+                        if !media.overview.isEmpty {
+                            CustomSectionView(title: "Overview") {
+                                Text(media.overview)
+                                    .font(.caption)
+                                    .padding(.horizontal)
                             }
                         }
-                        
+                        if !viewModel.mediaActors.isEmpty {
+                            CustomSectionView(title: "Actors") {
+                                ScrollView(.horizontal, showsIndicators: false) {
+                                    LazyHStack {
+                                        ForEach(viewModel.mediaActors) { mediaActor in
+                                            ActorCardView(actor: mediaActor)
+                                        }
+                                    }.padding([.horizontal, .bottom])
+                                }
+                            }
+                        }
+                        if !viewModel.mediaVideos.isEmpty {
+                            CustomSectionView(title: "Videos") {
+                                ScrollView(.horizontal, showsIndicators: false) {
+                                    LazyHStack {
+                                        ForEach(viewModel.mediaVideos) { mediaVideo in
+                                            YoutubeVideoView(mediaVideo.videoLink)
+                                        }
+                                    }.padding([.horizontal, .bottom])
+                                }
+                            }
+                        }
+                        if !viewModel.recommendedMedia.isEmpty {
+                            MediaListSection(mediaList: viewModel.recommendedMedia, sectionTitle: "You May Like", titleFont: .largeTitle, mediaType: viewModel.mediaType)
+                        }
                     }
                 }.coordinateSpace(name: "sticky")
                     .ignoresSafeArea(edges: .top)
@@ -78,8 +61,6 @@ struct DetailView<Model>: View where Model: DetailViewModelProtocol {
                         viewModel.handleMediaDetail()
                     }
             }
-        }.onAppear {
-            viewModel.handleMediaDetail()
         }
     }
 }
@@ -90,76 +71,36 @@ struct DetailView_Previews: PreviewProvider {
     }
 }
 
-protocol DetailViewModelProtocol: ObservableObject {
-    var mediaDetail: Media? { get }
-    var mediaActors: [Actor] { get }
-    
-    func handleMediaDetail()
-}
-
-class DetailViewModel: DetailViewModelProtocol {
-    @Published var mediaDetail: Media?
-    @Published var mediaActors: [Actor] = []
-    
-    var mediaId: Int
-    var mediaType: MediaTypes
-    
-    init(mediaId: Int, mediaType: MediaTypes) {
-        self.mediaId = mediaId
-        self.mediaType = mediaType
-    }
-    
-    func handleMediaDetail() {
-        let endpoint: String = mediaType == .tvShow ? ApiEndpoints.tvShowDetail.rawValue : ApiEndpoints.movieShowDetail.rawValue
-        
-        let url = NetworkManager.shared.createRequestURL(endpoint, pathVariables: [String(mediaId)], headerParams: [
-            "api_key": AppEnvironments.apiKey
-        ])
-        
-        NetworkManager.shared.apiRequest(endpoint: url) { response in
-            switch response {
-            case .success(let data):
-                guard let decodedData: Media = data.decodedModel() else { return }
-                DispatchQueue.main.async {
-                    self.mediaDetail = decodedData
+// MARK: - View Components
+extension DetailView {
+    func createImageHeaderInformationStack(media: Media) -> some View {
+        VStack {
+            HStack {
+                AnimatedAsyncImageView(path: media.posterImage)
+                    .frame(width: 40.0.responsizeW, height: 60.0.responsizeW)
+                    .transformEffect(CGAffineTransform(1, 0, 0, 1, 0, -10.0.responsizeW))
+                VStack(alignment: .leading) {
+                    Text(media.title)
+                        .lineLimit(4)
+                        .font(.system(size: 6.responsizeW, weight: .bold))
+                    Text(media.releaseYear)
+                        .font(.footnote)
+                    Spacer()
+                    if let mediaHomePage: URL = URL(string: media.mediaHomePage) {
+                        Button {
+                            openURL(mediaHomePage)
+                        } label: {
+                            LogoImageView(imagePath: media.mediaNetworks.first?.logoImagePath ?? "")
+                        }
+                    }
                 }
-            case .failure:
-                print("err")
-            }
+                Spacer()
+            }.padding(.horizontal)
+                .frame(height: 40.0.responsizeW)
+        }.overlay(alignment: .bottom) {
+            Divider()
+                .offset(y: 4.0.responsizeW)
+                .padding(.horizontal)
         }
-        
-        let actorsUrl = NetworkManager.shared.createRequestURL(endpoint, pathVariables: [
-            String(mediaId),
-            mediaType == .tvShow ? "aggregate_credits" : "credits"
-        ], headerParams: [
-            "api_key": AppEnvironments.apiKey
-        ])
-        
-        NetworkManager.shared.apiRequest(endpoint: actorsUrl) { response in
-            switch response {
-            case .success(let data):
-                guard let decodedData: ActorList = data.decodedModel() else { return }
-                DispatchQueue.main.async {
-                    self.mediaActors = decodedData.actorList
-                }
-            case .failure:
-                print("err")
-            }
-        }
-    }
-}
-
-struct LogoImageView: View {
-    @Environment(\.colorScheme) var colorScheme
-    private var backgroundColor: Color { colorScheme == .dark ? .white : .gray.opacity(0.1) }
-    let media: Media
-    
-    var body: some View {
-        HStack {
-            AnimatedAsyncImageView(path: media.mediaNetworks.first?.logoImagePath ?? "", cornerRadius: 0, scaleType: .toFit)
-                .frame(width: 10.0.responsizeW, height: 10.0.responsizeW)
-        }.padding(1.0.responsizeW)
-            .background(backgroundColor)
-            .cornerRadius(5)
     }
 }
---
+++ SwiftUI-MVVM-Example/Modules/Detail/DetailViewModel.swift
@@ -0,0 +1,110 @@
+//
+//  DetailViewModel.swift
+//  SwiftUI-MVVM-Example
+//
+//  Created by Mehmet Ateş on 11.11.2022.
+//
+
+import Foundation
+
+protocol DetailViewModelProtocol: ObservableObject {
+    var mediaDetail: Media? { get }
+    var mediaActors: [Actor] { get }
+    var mediaVideos: [MediaVideos] { get }
+    var recommendedMedia: [Media] { get }
+    var mediaType: MediaTypes { get }
+    
+    func handleMediaDetail()
+}
+
+class DetailViewModel: DetailViewModelProtocol {
+    @Published var mediaDetail: Media?
+    @Published var mediaActors: [Actor] = []
+    @Published var mediaVideos: [MediaVideos] = []
+    @Published var recommendedMedia: [Media] = []
+    
+    var mediaId: Int
+    var mediaType: MediaTypes
+    
+    init(mediaId: Int, mediaType: MediaTypes) {
+        self.mediaId = mediaId
+        self.mediaType = mediaType
+    }
+    
+    func handleMediaDetail() {
+        let endpoint: String = mediaType == .tvShow ? ApiEndpoints.tvShowDetail.rawValue : ApiEndpoints.movieShowDetail.rawValue
+        
+        let url = NetworkManager.shared.createRequestURL(endpoint, pathVariables: [String(mediaId)], headerParams: [
+            "api_key": AppEnvironments.apiKey
+        ])
+        
+        NetworkManager.shared.apiRequest(endpoint: url) { response in
+            switch response {
+            case .success(let data):
+                guard let decodedData: Media = data.decodedModel() else { return }
+                DispatchQueue.main.async {
+                    self.mediaDetail = decodedData
+                }
+            case .failure:
+                print("err")
+            }
+        }
+        
+        let actorsUrl = NetworkManager.shared.createRequestURL(endpoint, pathVariables: [
+            String(mediaId),
+            mediaType == .tvShow ? "aggregate_credits" : "credits"
+        ], headerParams: [
+            "api_key": AppEnvironments.apiKey
+        ])
+        
+        NetworkManager.shared.apiRequest(endpoint: actorsUrl) { response in
+            switch response {
+            case .success(let data):
+                guard let decodedData: ActorList = data.decodedModel() else { return }
+                DispatchQueue.main.async {
+                    self.mediaActors = decodedData.actorList
+                }
+            case .failure:
+                print("err")
+            }
+        }
+        
+        let videosUrl = NetworkManager.shared.createRequestURL(endpoint, pathVariables: [
+            String(mediaId),
+            "videos"
+        ], headerParams: [
+            "api_key": AppEnvironments.apiKey
+        ])
+        
+        NetworkManager.shared.apiRequest(endpoint: videosUrl) { response in
+            switch response {
+            case .success(let data):
+                guard let decodedData: MediaVideoList = data.decodedModel() else { return }
+                DispatchQueue.main.async {
+                    self.mediaVideos = decodedData.mediaVideoList
+                }
+            case .failure:
+                print("err")
+            }
+        }
+        
+        let recommendedURL = NetworkManager.shared.createRequestURL(endpoint, pathVariables: [
+            String(mediaId),
+            "recommendations"
+        ], headerParams: [
+            "api_key": AppEnvironments.apiKey
+        ])
+        
+        NetworkManager.shared.apiRequest(endpoint: recommendedURL) { response in
+            switch response {
+            case .success(let data):
+                guard let decodedData: MediaList = data.decodedModel() else { return }
+                DispatchQueue.main.async {
+                    self.recommendedMedia = decodedData.mediaList
+                }
+            case .failure:
+                print("err")
+            }
+        }
+    }
+}
