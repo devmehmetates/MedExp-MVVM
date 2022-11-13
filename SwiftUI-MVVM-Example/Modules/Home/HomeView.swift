e63af86751b05249a8d9c68da56e9a6f31771976
Mehmet Ateş  <Mehmet1095@icloud.com>
2022-11-13 14:39:44 +0300

    refactoring on home view

--- SwiftUI-MVVM-Example/Modules/Home/HomeView.swift
+++ SwiftUI-MVVM-Example/Modules/Home/HomeView.swift
@@ -14,25 +14,36 @@ struct HomeView<Model>: View where Model: HomeViewModelProtocol {
         NavigationView {
             Group {
                 if viewModel.isPageLoaded {
-                    ScrollView {
-                        if let headerMediaList: [Media] = viewModel.mediaSections["Popular on TV"]?.mediaList {
-                            HeaderCarouselMediaList(mediaList: headerMediaList)
-                        }
-                        LazyVStack {
-                            ForEach(Array(viewModel.mediaSections.keys).sorted(), id: \.self) { sectionKey in
-                                if let mediaSectionValue: MediaSectionValue = viewModel.mediaSections[sectionKey] {
-                                    MediaListSection(mediaList: mediaSectionValue.mediaList, sectionTitle: sectionKey, mediaType: mediaSectionValue.type) {
-                                        viewModel.increasePage(withSectionKey: sectionKey)
-                                    }
-                                }
-                            }
-                        }
-                    }
+                    loadedState
                 } else {
                     loadingState
                 }
             }.navigationTitle("What's Popular")
         }.tag("Home")
+    }
+}
+
+// MARK: - View Component(s)
+extension HomeView {
+    private var loadedState: some View {
+        ScrollView {
+            if let headerMediaList: [Media] = viewModel.mediaSections["Popular on TV"]?.mediaList {
+                HeaderCarouselMediaList(mediaList: headerMediaList)
+            }
+            movieSectionStacks
+        }
+    }
+    
+    private var movieSectionStacks: some View {
+        LazyVStack {
+            ForEach(Array(viewModel.mediaSections.keys).sorted(), id: \.self) { sectionKey in
+                if let mediaSectionValue: MediaSectionValue = viewModel.mediaSections[sectionKey] {
+                    MediaListSection(mediaList: mediaSectionValue.mediaList, sectionTitle: sectionKey, mediaType: mediaSectionValue.type) {
+                        viewModel.increasePage(withSectionKey: sectionKey)
+                    }
+                }
+            }
+        }
     }
 }
 
